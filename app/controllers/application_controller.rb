305af70a79dc3582bdfde4b31c8e5980eb5a020f
rkanjani <rkanj017@uottawa.ca>
2016-11-30 20:06:45 -0500

    more fie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:type, :given_name, :last_name])
+  end
+
 end
