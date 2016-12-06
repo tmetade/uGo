00893fef0012ba39f3aaf4aa0d38f816695fa3c6
rkanjani <rkanj017@uottawa.ca>
2016-12-06 11:28:44 -0500

    Provided feedback on apps

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -2,4 +2,8 @@ class Recommendation < ApplicationRecord
 	 belongs_to :application 
 	
 	 validates :application_recommendation, :presence => true
+
+	 def comments
+	 	return self.application_recommendation
+	 end	
 end
